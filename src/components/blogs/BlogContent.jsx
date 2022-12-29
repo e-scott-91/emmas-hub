8877b9c1c6edc9b7e46f99288da30c9a7e6ed803
escott7 <emma_scott@intuit.com>
2022-12-29 17:16:44 +0000

    feat: arrange initial blog posts

--- src/components/blogs/BlogContent.jsx
+++ src/components/blogs/BlogContent.jsx
@@ -12,6 +12,20 @@ export default function BlogContent() {
             <p>Over time I've blogged about a variety of things in different
             formats but the focus here is my journey from a backend engineer to a
             full stack engineer.</p>
+            <div className="blog-posts">
+                <div className="blog-post">
+                    <h2>First Blog Post</h2>
+                    <p>Click here to view my first blog post</p>
+                </div>
+                <div className="blog-post">
+                    <h2>Second Blog Post</h2>
+                    <p>Click here to view my second blog post</p>
+                </div>
+                <div className="blog-post">
+                    <h2>Third Blog Post</h2>
+                    <p>Click here to view my third blog post</p>
+                </div>
+            </div>
         </div>
     )
 }
