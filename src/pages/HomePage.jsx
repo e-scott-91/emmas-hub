ad84ee78d8d0ce7640387ad182d408ded9373c57
escott7 <emma_scott@intuit.com>
2023-01-02 15:49:42 +0000

    feat: change from avatar

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import HomePageInfo from '../components/home/HomePageInfo';
-import avatar from '../assets/avatar.png';
+import profPic from '../assets/prof-pic.jpeg';
+import Paper from '@mui/material/Paper';
 import './HomePage.css';
 
 function HomePage() {
@@ -9,7 +10,9 @@ function HomePage() {
             <div className="home-page-info">
                 <HomePageInfo/>
             </div>
-            <img src={avatar} className="avatar" alt="Avatar of Emma" />
+            <Paper elevation={3} sx={{height:"550px", width:"425px",display:"flex", flexDirection:"column",justifyContent:"center", alignItems:"center" }}>
+                <img src={profPic} className="avatar" alt="Avatar of Emma" />
+            </Paper>
         </div>
     )
 }
